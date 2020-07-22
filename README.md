6405c1ea96ec65a8bc7bd2090304df09074556de
biplob14 <biploburrahaman@gmail.com>
2020-07-23 02:28:14 +0600

    A little parenthesis were making trouble

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # simple-nodejs-server
+This code is the symbol of my node js beginning 
+Using this code can create a simple localhost server to access in this specific file.
--- app.js
+++ app.js
@@ -1,9 +1,6 @@
 var http = require('http');
 
-http.createServer(function  (req, res)) {
-    res.writeHead(200, {'contentType': 'text/plain'});
-    res.end('Hello world!');
-
-)}.listen(8080);
-
-var http = require('http');
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello World!');
+}).listen(8080);
